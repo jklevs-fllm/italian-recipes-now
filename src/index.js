a1d89190de3ba27a5bb1f2b9e8057bdff3997f9f
jklevs-fllm <jklevs@hotmail.com>
2026-01-04 12:12:18 +0100

    Added basic java script and added typewriter plug-in

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateRecipe(event) {
+  event.preventDefault();
 
+  new Typewriter("#recipe", {
+    strings: "TRADITIONAL CARBONARA",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let recipeFormElement = document.querySelector("#recipe-form");
+recipeFormElement.addEventListener("submit", generateRecipe);
