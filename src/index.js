8e0a9d0f69991a24406d639dad79edb18a597e40
jklevs-fllm <jklevs@hotmail.com>
2026-01-04 13:01:24 +0100

    Added API

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-function generateRecipe(event) {
-  event.preventDefault();
-
+function displayRecipe(response) {
+  console.log("recipe generated");
   new Typewriter("#recipe", {
-    strings: "TRADITIONAL CARBONARA",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generateRecipe(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "b42tc14aec3cob92cf84464239608fca";
+
+  let context =
+    "You are a skilled Italian chef and love to tell people about traditional, Italian recipes for them to cook at home. Make sure you follow the user instructions and include this as one of the ingredients in the recipe you provide.";
+
+  let prompt = `User Instructions:Generate a recipe about ${instructionsInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating Recipe");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiUrl).then(displayRecipe);
 }
 
 let recipeFormElement = document.querySelector("#recipe-form");
