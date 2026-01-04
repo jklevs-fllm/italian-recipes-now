b6196e30cbc70228db49c63c083d4198c3a422ee
jklevs-fllm <jklevs@hotmail.com>
2026-01-04 18:32:30 +0100

    Added blinking animation, updated prompts and context to API

--- src/index.js
+++ src/index.js
@@ -15,10 +15,14 @@ function generateRecipe(event) {
   let apiKey = "b42tc14aec3cob92cf84464239608fca";
 
   let context =
-    "You are a skilled Italian chef and love to tell people about traditional, Italian recipes for them to cook at home. Make sure you follow the user instructions and include this as one of the ingredients in the recipe you provide.";
+    "You are a skilled Italian chef and love to tell people about traditional, Italian recipes for them to cook at home. Make sure you follow the user instructions and include this as one of the ingredients in the recipe you provide. Provide the recipe in basic HTML format that is easy to follow, using two headings INGREDIENTS and METHOD.";
 
-  let prompt = `User Instructions:Generate a recipe about ${instructionsInput.value}`;
+  let prompt = `User Instructions:Generate a recipe about ${instructionsInput.value} `;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let recipeElement = document.querySelector("#recipe");
+  recipeElement.classList.remove("hidden");
+  recipeElement.innerHTML = `<div class = "blink">⏲ Finding a fabulous recipe for you using <strong>${instructionsInput.value}! </strong></div> `;
 
   console.log("Generating Recipe");
   console.log(`Prompt: ${prompt}`);
